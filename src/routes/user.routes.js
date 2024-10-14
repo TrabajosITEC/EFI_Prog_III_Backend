4c836d6cb1e2a8b08b885d13f7133e0e9fe721aa
juantobares4 <j.tobares@itecriocuarto.org.ar>
2024-10-14 13:48:59 -0300

    filter users by id

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { getUserRegister, postUsersRegister } from "../controllers/user.controllers.js";
+import { getUserRegister, postUsersRegister, getUserById } from "../controllers/user.controllers.js";
 
 const routerRegister = Router()
 
 routerRegister.get('/', getUserRegister);
 
 routerRegister.post('/', postUsersRegister);
+
+routerRegister.get('/:id', getUserById);
 
 
 export default routerRegister
