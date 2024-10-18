a38f4e41dd0d0b745ad75bbbeee56450837e57f7
juantobares4 <j.tobares@itecriocuarto.org.ar>
2024-10-18 10:19:15 -0300

    get authenticated user profile

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,53 +1,72 @@
 import db from "../../models/index.js";
 
 export const getUserRegister = async (req, res) => {
-    try {
-        const userRegisters = await db.User.findAll();
-        res.json(userRegisters);
-    } catch (error) {
-        console.error("Error:", error)
-    }
-}
+  try {
+    const userRegisters = await db.User.findAll();
+    res.json(userRegisters);
+  } catch (error) {
+    console.error("Error:", error)
+
+  };
+
+};
 
 export const postUsersRegister = async (req, res) => {
-    const { userName, passWord, email } = req.body;
-    try {
-        const newUser = await db.User.create(
-            {
-                userName: userName,
-                passWord: passWord,
-                email: email,
-                role: 'gamer'
-            },
-        )
-        res.status(201).json({ message: 'Usuario creado exitosamente' })
-    } catch (error) {
-        return res.status(500).json({ error: 'Error al insertar base de datos' })
-    }
+  const { userName, passWord, email } = req.body;
+  
+  try {
+    const newUser = await db.User.create(
+      {
+        userName: userName,
+        passWord: passWord,
+        email: email,
+        role: 'gamer'
+      },
+    )
+    res.status(201).json({ message: 'Usuario creado exitosamente' })
+ 
+  } catch (error) {
+    return res.status(500).json({ error: 'Error al insertar base de datos' })
+  
+  };
+
 };
 
 export const getUserById = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const findUser = await db.User.findByPk(id);
+  try {
+    const { id } = req.params;
+    const findUser = await db.User.findByPk(id);
 
-        if (findUser) {
-            try {
-                return res.status(200).json({ 'Usuario encontrado': findUser });
+    if (findUser) {
+      try {
+        return res.status(200).json({ 'Usuario encontrado': findUser });
 
-            } catch (error) {
-                return res.status(500).json({ message: error });
-
-            }
-
-        } else {
-            return res.status(404).json({ message: `El usuario con ID ${id} no existe.` });
-
-        };
-
-    } catch (error) {
+      } catch (error) {
         return res.status(500).json({ message: error });
 
+      }
+
+    } else {
+      return res.status(404).json({ message: `El usuario con ID ${id} no existe.` });
+
     };
+
+  } catch (error) {
+    return res.status(500).json({ message: error });
+
+  };
+
+};
+
+export const getAuthUser = async (req, res) => {
+  try {
+    const currentUser = req.user;
+    
+    return res.status(200).json({ "Usuario actual": currentUser});
+
+  } catch (error) {
+    return res.status(500).json('Error al obtener los datos del usuario.');
+
+  };
 
 };
