a688299c9689523f54033e7c50341d9defa260e2
Joaquin Martinez <jo.martinez@itecriocuarto.org.ar>
2024-11-04 23:03:06 -0300

    seeders

--- seeders/20241008015727-create-reviews.cjs
+++ seeders/20241008015727-create-reviews.cjs
@@ -4,15 +4,26 @@
 export async function up(queryInterface, Sequelize) {
 
   await queryInterface.bulkInsert('Review', [{
-    game_id: 2,
+    game_id: 1,
     user_id: 1,
-    rating: 5,
-    comment: "burritoOrtega"
+    rating: 1,
+    comment: "Neymar esta roto!!"
   }, {
-    game_id: 4,
+    game_id: 6,
     user_id: 2,
-    rating: 4,
-    comment: null
+    rating: 5,
+    comment: "Juegazo, gran impactrueno!!"
+  },
+  {
+    game_id: 1,
+    user_id: 2,
+    rating: 1,
+    comment: "Aguante el PES"
+  },{
+    game_id: 1,
+    user_id: 1,
+    rating: 1,
+    comment: "Y el winning eleven!!"
   },
   ], {});
 
